537878d6709810274dcd2bb3eae37836e28575bb
Eric Galway <ericwilliamgalway@gmail.com>
2021-12-14 21:07:43 -0500

    Added vertical floating movement to platform

--- Assets/[Scripts]/ShrinkingPlatform.cs
+++ Assets/[Scripts]/ShrinkingPlatform.cs
@@ -1,37 +1,63 @@
+//------------------ShrinkingPlatform.cs------------------
+//------------------Author: Eric Galway ------------------
+//------------------Student Number: 101252535-------------
+//--------Description: shrinking platform controller------
+//--------controls the shrinking and expansion of the ----
+//--------floating platforms. Also bobs them up and down--
+//--------Last Edited: December 14 2021-------------------
+//--------Revision History: 1.5 Added vertical bobbing ---
+
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//Enumeration for determining shrinking state
 public enum ShrinkState
 {
     EXPANDING,
     SHRINKING,
 }
 
-
+//Shrinking platform script
 public class ShrinkingPlatform : MonoBehaviour
 {
     public LayerMask playerLayerMask;
+    public Rigidbody2D rigidBody;
     public float startingWidthScale;
     public float expandSpeed;
     public bool isColliding;
     public AudioSource[] audioSources;
+    public float floatDistance;
+    public float floatSpeed;
+    public float direction;
+    private float distanceTravelled;
 
     // Start is called before the first frame update
     void Start()
     {
+        //Get the starting scale so that we know the value to return to
         startingWidthScale = transform.localScale.x;
     }
 
     // Update is called once per frame
     void Update()
     {
-        CheckCollision();
+        //Moves the platform up and down
+        BobPlatform();
     }
 
-    void CheckCollision()
+    void BobPlatform()
     {
-        //collider.GetContacts()
+        float amountToMove = transform.position.y + direction * floatSpeed * Time.deltaTime;
+        //Only want to travel on y axis
+        transform.position = new Vector3(transform.position.x, amountToMove, transform.position.z);
+        distanceTravelled += floatSpeed * Time.deltaTime;
+        if (distanceTravelled >= floatDistance)
+        {
+            distanceTravelled = 0;
+            direction *= -1;
+        }
     }
 
     private void ShrinkPlatform()
@@ -46,23 +72,30 @@ public class ShrinkingPlatform : MonoBehaviour
 
     IEnumerator Disappeared()
     {
+        //Wait briefly for player to fall before expanding again
         yield return new WaitForSecondsRealtime(0.5f);
         yield return StartCoroutine(Expand());
     }
-
+    //Reduces the size of the platform up to 0 using a corountien
     IEnumerator Shrink()
     {
         PlaySound(ShrinkState.SHRINKING);
         while (transform.localScale.x > 0)
         {
+            //If we stop colliding we want to exit out of this early 
             if (!isColliding)
                 yield break;
+
             float scale = transform.localScale.x - expandSpeed * Time.deltaTime;
             transform.localScale = new Vector3(scale, transform.localScale.y, transform.localScale.z);
+
             yield return null;
         }
+        //Ensure if reduce scale to a negative number through subtraction we set it to 0
         transform.localScale = new Vector3(0, transform.localScale.y, transform.localScale.z);
 
+        //This is how we notify that the player isnt colliding with a 0 x scale platform
+        //need this cause OnCollisionExit2D will not detect leaving collision when it turns to 0
         if (transform.localScale.x == 0)
         {
             isColliding = false;
@@ -72,26 +105,34 @@ public class ShrinkingPlatform : MonoBehaviour
             yield return null;
     }
  
-
+    //Expands the platform using a coroutine
     IEnumerator Expand()
     {
+        //Want to wait slightly for player to fall or jump etc 
         yield return new WaitForSecondsRealtime(0.1f);
         PlaySound(ShrinkState.EXPANDING);
         isColliding = false;
         while (transform.localScale.x < startingWidthScale)
         {
+            //Want to exit early if we are colliding again
             if (isColliding)
                 yield break;
+
             float scale = transform.localScale.x + expandSpeed * Time.deltaTime;
             transform.localScale = new Vector3(scale, transform.localScale.y, transform.localScale.z);
+
             yield return null;
         }
+        //Want to ensure that we get back to exact starting value
         transform.localScale = new Vector3(startingWidthScale, transform.localScale.y, transform.localScale.z);
 
     }
 
+    //Sometimes collision wasnt being detected by OnEnter so used this instead. Only fires
+    //when we aren't already colliding so does the same thing just checks more often
     private void OnCollisionStay2D(Collision2D collision)
     {
+        //Only want to shrink platform when we are colliding with the top of it
         int layer = collision.gameObject.layer;
         if ((layer == layer << playerLayerMask.value) && collision.contacts[0].normal == Vector2.down)
         {
@@ -100,10 +141,10 @@ public class ShrinkingPlatform : MonoBehaviour
                 isColliding = true;
                 ShrinkPlatform();
             }
-
         }
     }
 
+    //Checks if we are no longer colliding with the platform
     private void OnCollisionExit2D(Collision2D collision)
     {
         int layer = collision.gameObject.layer;
